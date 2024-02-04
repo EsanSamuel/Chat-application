d8ce36bb5d89c3bf9b2a2296e1aca6bfaafe3da7
Esan Samuel <esansamuel555@gmail.com>
2024-02-03 17:30:00 -0800

    Update protectRoute.js

--- middleware/protectRoute.js
+++ middleware/protectRoute.js
@@ -20,7 +20,7 @@ const protectRoute = async (req, res, next) => {
 			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
 		}
 
-//finding decoded user by id and 
+//finding decoded user by id and removing password 
 		const user = await User.findById(decoded.userId).select("-password");
 
 		if (!user) {
